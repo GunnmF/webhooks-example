2c6b69cd9c95ad3252204f7c4f9f090f7ba0545b
fanchen <moumou.v1@foxmail.com>
2023-04-26 21:14:03 +0800

    update

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
  * @Description:
  * @Author: moumou.v1@foxmail.com
  * @Date: 2023-04-25 18:49:18
- * @LastEditTime: 2023-04-26 20:41:35
+ * @LastEditTime: 2023-04-26 21:14:00
  * @LastEditors: moumou.v1@foxmail.com
  */
 const express = require('express')
+const crypto = require('crypto')
 const app = express()
-// const
 const port = 3001
+const SECRET = '123456'
+function sign(body) {
+  return `sha1=${crypto.createHmac('sha1', SECRET).update(body).digest('hex')}`
+}
 
 // 跨域设置
 app.all('*', function (req, res, next) {
@@ -30,7 +34,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.post('/api/webhooks', (req, res) => {
-  console.log('webhook', res,req)
+  // console.log('webhook', res,req)
+  let buffers = []
+  res.on('data', (data) => {
+    buffers.push(data)
+  })
+  res.on('end', () => {
+    // 这里发送结束条件，并处理数据。可以将处理的数据
+    let body = Buffer.concat(buffers) // 转换为字符串并将其存储在变量中。
+    let event = req.headers['x-github-event']
+    let signature = req.headers['x-hub-signature']
+    console.log(body, event, signature)
+    if (signature !== sign(body)) {
+      return res.send('Not Allowed')
+    }
+  })
   res.setHeader('Content-Type', 'application/json')
   res.send(
     JSON.stringify({
