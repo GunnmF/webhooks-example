ace1e8f20ca6eb02af3961a8c785b9d5b0e6da10
fanchen <moumou.v1@foxmail.com>
2023-04-28 00:37:51 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: moumou.v1@foxmail.com
  * @Date: 2023-04-25 18:49:18
- * @LastEditTime: 2023-04-28 00:35:55
+ * @LastEditTime: 2023-04-28 00:37:44
  * @LastEditors: moumou.v1@foxmail.com
  */
 
@@ -53,12 +53,12 @@ app.post('/api/webhooks', (req, res) => {
     }
     console.log('签名校验成功')
     if (event === 'push') {
-      let { repository } = JSON.parse(body)
-      console.log('仓库:', repository.name)
-      console.log('提交人:', repository)
-      console.log('邮箱:', head_commit)
+      let payload= JSON.parse(body)
+      console.log('仓库:', payload.repository.name)
+      console.log('提交人:', payload)
+      // console.log('邮箱:', head_commit)
       console.log('执行脚本构建镜像')
-      executeSh(repository.name)
+      executeSh(payload.repository.name)
     }
     res.send(
       JSON.stringify({
