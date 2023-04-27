c44a19d334e08191421022e8f7d195a1ca955b66
fanchen <moumou.v1@foxmail.com>
2023-04-28 00:33:05 +0800

    update

--- webhooks.sh
+++ webhooks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 WORK_PATH='/app/webhooks-example'
 cd $WORK_PATH
+echo "停止服务"
+pm2 stop webhooks
+pm2 delete webhooks
 echo "清理代码"
 git reset --hard origin/main
 git clean -f
@@ -8,3 +11,5 @@ echo "拉取代码"
 git pull origin main
 echo "安装依赖"
 pnpm i
+echo "启动服务"
+pnpm run dev
