75a676a077676dec56eae80bc33477750b7732ed
fanchen <moumou.v1@foxmail.com>
2023-04-28 11:34:33 +0800

    update

--- utils.js
+++ utils.js
@@ -2,22 +2,28 @@
  * @Description:
  * @Author: moumou.v1@foxmail.com
  * @Date: 2023-04-27 19:54:06
- * @LastEditTime: 2023-04-28 11:18:10
+ * @LastEditTime: 2023-04-28 11:34:13
  * @LastEditors: moumou.v1@foxmail.com
  */
 const { createHmac } = require('crypto')
 const { exec } = require('child_process')
 const { join } = require('path')
+const jwt = require('jsonwebtoken')
 const { SECRET, REPOSITORY } = require('./config')
-
 // 生成签名
 const generateSign = (body) => {
   const timestamp = new Date().getTime()
   const secret = 'this is secret'
   const stringToSign = `${timestamp}\n${secret}`
-  const hmacCode = createHmac('sha256', secret).update(stringToSign).digest('hex')
+  const hmacCode = createHmac('sha256', secret)
+    .update(stringToSign)
+    .digest('hex')
   const sign = encodeURIComponent(btoa(hmacCode))
-  console.log(timestamp)
+  const signature = `sha256=${crypto
+    .createHmac('sha256', SECRET)
+    .update(body)
+    .digest('hex')}`
+  console.log(timestamp, signature)
   console.log(sign)
   return `sha1=${createHmac('sha1', SECRET).update(body).digest('hex')}`
 }
@@ -36,6 +42,13 @@ const executeSh = (repositoryName) => {
       console.log('脚本执行完毕')
     }
   )
+}
+
+function signRequestBody(secret, body) {
+  const header = { alg: 'HS256', typ: 'JWT' }
+  const payload = { payload: body }
+  const token = jwt.sign(payload, secret, { algorithm: 'HS256', header })
+  return `Bearer ${token}`
 }
 
 module.exports = { generateSign, executeSh }
